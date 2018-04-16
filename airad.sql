faa7ecbbe850bba3e343945960639f0f3be553f2
rubinliudongpo <rubinliudongpo@users.noreply.github.com>
2018-04-16 11:51:46 +0800

    added airad.sql;updated README

---
+++ airad.sql
@@ -0,0 +1,189 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: airad
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `air_ad`
+--
+
+DROP TABLE IF EXISTS `air_ad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `air_ad` (
+  `id` int(11) NOT NULL,
+  `device_id` int(11) NOT NULL DEFAULT '0',
+  `created_at` bigint(20) NOT NULL DEFAULT '0',
+  `nh3` varchar(4) NOT NULL DEFAULT '',
+  `co` varchar(4) NOT NULL DEFAULT '',
+  `o3` varchar(4) NOT NULL DEFAULT '',
+  `pm25` varchar(4) NOT NULL DEFAULT '',
+  `pm10` varchar(4) NOT NULL DEFAULT '',
+  `so2` varchar(4) NOT NULL DEFAULT '',
+  `temperature` varchar(4) NOT NULL DEFAULT '',
+  `humidity` varchar(4) NOT NULL DEFAULT '',
+  `aqi_quality` varchar(4) NOT NULL DEFAULT '',
+  `suggest` varchar(4) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `air_ad`
+--
+
+LOCK TABLES `air_ad` WRITE;
+/*!40000 ALTER TABLE `air_ad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `air_ad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `device`
+--
+
+DROP TABLE IF EXISTS `device`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `device_name` varchar(32) NOT NULL DEFAULT '',
+  `address` varchar(50) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `created_at` bigint(20) NOT NULL DEFAULT '0',
+  `updated_at` bigint(20) NOT NULL DEFAULT '0',
+  `latitude` varchar(12) NOT NULL DEFAULT '',
+  `longitude` varchar(12) NOT NULL DEFAULT '',
+  `airad_count` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `device_name` (`device_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device`
+--
+
+LOCK TABLES `device` WRITE;
+/*!40000 ALTER TABLE `device` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mqtt`
+--
+
+DROP TABLE IF EXISTS `mqtt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mqtt` (
+  `id` int(11) NOT NULL,
+  `device_id` int(11) NOT NULL DEFAULT '0',
+  `mqtt_option_id` int(11) NOT NULL DEFAULT '0',
+  `qos` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `retain` tinyint(1) NOT NULL DEFAULT '0',
+  `topic_name` varchar(255) NOT NULL DEFAULT '',
+  `topic_filter` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mqtt`
+--
+
+LOCK TABLES `mqtt` WRITE;
+/*!40000 ALTER TABLE `mqtt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mqtt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mqtt_option`
+--
+
+DROP TABLE IF EXISTS `mqtt_option`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mqtt_option` (
+  `id` int(11) NOT NULL,
+  `client_id` varchar(255) NOT NULL DEFAULT '',
+  `conn_ack_timeout` bigint(20) NOT NULL DEFAULT '0',
+  `user_name` varchar(32) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `clean_session` tinyint(1) NOT NULL DEFAULT '0',
+  `keep_alive` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `will_topic` varchar(255) NOT NULL DEFAULT '',
+  `will_message` varchar(255) NOT NULL DEFAULT '',
+  `will_qos` tinyint(1) NOT NULL DEFAULT '0',
+  `will_retain` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mqtt_option`
+--
+
+LOCK TABLES `mqtt_option` WRITE;
+/*!40000 ALTER TABLE `mqtt_option` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mqtt_option` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `salt` varchar(128) NOT NULL DEFAULT '',
+  `token` varchar(256) NOT NULL DEFAULT '',
+  `gender` int(11) NOT NULL DEFAULT '0',
+  `age` int(11) NOT NULL DEFAULT '0',
+  `address` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `last_login` bigint(20) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `created_at` bigint(20) NOT NULL DEFAULT '0',
+  `updated_at` bigint(20) NOT NULL DEFAULT '0',
+  `device_count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-16 11:49:30
